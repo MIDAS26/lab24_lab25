b7e05d34ed76d1b899778559756b2bbd6e9cbcbd
MIDAS26 <56016042+MIDAS26@users.noreply.github.com>
2022-07-04 17:15:22 +0300

    Add files via upload

---
+++ help_menu.py
@@ -0,0 +1,21 @@
+from tkinter import *
+from tkinter.messagebox import *
+
+
+class Help():
+    def about(root):
+        showinfo(title="About", message="This is a text editor made using Python for Lab24 and Lab25")
+
+
+def main(root, text, menubar):
+    help = Help()
+
+    helpMenu = Menu(menubar)
+    helpMenu.add_command(label="About", command=help.about)
+    menubar.add_cascade(label="Help", menu=helpMenu)
+
+    root.config(menu=menubar)
+
+
+if __name__ == "__main":
+    print("Please run 'main.py'")
